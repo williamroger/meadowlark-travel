05163cae24f31a5e29f5f22f6f564e1658309605
William Roger <williamroger.frontend@gmail.com>
2020-06-03 01:30:11 -0300

    Separando os manipuladores de rotas

--- meadowlark.js
+++ meadowlark.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const expressHandlebars = require('express-handlebars');
 
-const fortune = require('./lib/fortune');
+const handlers = require('./lib/handlers');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -15,24 +15,15 @@ app.set('view engine', 'handlebars');
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', (req, res) => res.render('home'));
+app.get("/", handlers.home);
 
-app.get('/about', (req, res) => {
-  res.render("about", { fortune: fortune.getFortune() });
-});
+app.get("/about", handlers.about);
 
 // página 404 personalizada
-app.use((req, res) => {
-  res.status(404);
-  res.render('404');
-});
+app.use(handlers.notFound);
 
 // página 505 personalizada
-app.use((err, req, res, next) => {
-  console.error(err.message);
-  res.status(505);
-  res.render('505');
-});
+app.use(handlers.serverError);
 
 app.listen(port, () => {
   console.log(`Express started on http://localhost:${port}; ` + 'press Crtl-c to terminate...');
